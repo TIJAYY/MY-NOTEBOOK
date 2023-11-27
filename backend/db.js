0a43e2f82f9379e9fd6f9cbbea0eff586a24f04b
Tushar Jangra <that.tijay@gmail.com>
2023-11-27 17:18:50 +0530

    mongoDb updated

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // const mongoURI = "mongodb://localhost:27017"
-const mongoURI = "mongodb+srv://Tushar:0c2IyUfy52Rklq0s@inotebook.xatgygn.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv://tushar:D6lES97EtpA6B2nU@cluster0.uskb5cb.mongodb.net/"
 //add your own mongoURI to make it work on your device
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
